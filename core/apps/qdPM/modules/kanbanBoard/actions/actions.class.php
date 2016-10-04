c977ac7baf4c38c88487e5c780f6c6bb1fe718dd
ninearif <ninearif@hotmail.com>
2016-10-05 00:41:47 +0700

    create kanbanBoard module

---
+++ core/apps/qdPM/modules/kanbanBoard/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * kanbanBoard actions.
+ *
+ * @package    sf_sandbox
+ * @subpackage kanbanBoard
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class kanbanBoardActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
